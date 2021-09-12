2291f63de2064fd3e4204b1ab9c72f1a1dad1d8a
Pingu1337 <herman.brunberg@utb.ecutbildning.se>
2021-09-12 17:48:59 +0200

    +NoStore cache for HttpClient

--- pingu.onl/Program.cs
+++ pingu.onl/Program.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace pingu.onl
 {
@@ -17,7 +18,15 @@ namespace pingu.onl
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            builder.Services.AddScoped(sp => new HttpClient(new DefaultBrowserOptionsMessageHandler(new HttpClientHandler())
+            {
+                DefaultBrowserRequestCache = BrowserRequestCache.NoStore,
+                DefaultBrowserRequestCredentials = BrowserRequestCredentials.Include,
+                DefaultBrowserRequestMode = BrowserRequestMode.Cors,
+            })
+            {
+                BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+            });
 
             await builder.Build().RunAsync();
         }
